88488035092e1e209dd08ed57da97eac61150a48
Huy Bui <anhhuybk@gmail.com>
2014-09-30 15:59:55 -0500

    still working on dat and cmd for qos

--- model/qos/optiq.cmd
+++ model/qos/optiq.cmd
@@ -1,5 +1,5 @@
 model optiq.mod;
-data optiq32.dat;
+data optiq.dat;
 option solver snopt;
 solve;
 
@@ -10,12 +10,16 @@ param JBS:=card(Jobs)-1;
 var k:=0;
 
 for {job in Jobs} {
-    printf "J %d %d %d %8.0f\n", k, Source[job], Destination[job], Demand[job];
-    for {(u,v) in Arcs} {
-	if Flow[k,u,v] > 0.0005 then
-	    printf "%4d %4d %8.0f\n", u, v, Flow[k,u,v];
+    for {t in Types} {
+	if Demand[job,t] >0 then {
+	    printf "Demand: %d %d %s %8.0f\n", Source[job], Destination[job], t, Demand[job,t];
+	    printf "   Flows:\n";
+	    for {(u,v) in Arcs} {
+		if Flow[k,t,u,v] > 0.0005 then
+		    printf "%4d %4d %8.0f\n", u, v, Flow[k,t,u,v];
+	    }
+	}
     }
     let k:=k+1;
     printf "\n";
 }
-
