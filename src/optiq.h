d9cc7c3c3385a3927deda3c6fd893fe005ba6df5
Huy Bui <anhhuybk@gmail.com>
2014-10-22 15:36:38 -0700

    more edits

--- src/optiq.h
+++ src/optiq.h
@@ -1,9 +1,15 @@
 #ifndef OPTIQ_H
 #define OPTIQ_H
 
+#include <core/system/topology.h>
+#include <core/structures/graph.h>
+#include <model/request.h>
+#include <engine/transport.h>
+
 struct optiq {
-    struct optiq_info info;
-    struct optiq_interface impl;
-}
+    machine_type machine;
+};
+
+void optiq_init();
 
 #endif
