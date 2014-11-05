e92dcee347dd02b0a0cf989eea0974dc43a2f2a0
Huy Bui <huybui@anl.gov>
2014-11-05 23:19:28 +0000

    more changes

--- src/core/structures/flow.h
+++ src/core/structures/flow.h
@@ -1,12 +1,34 @@
 #ifndef OPTIQ_FLOW
 #define OPTIQ_FLOW
 
-struct flow {
+#include <queue>
+
+using namespace std;
+
+struct optiq_arc {
+    int ep1;
+    int ep2;
+};
+
+struct optiq_flow {
+    int id;
+    int throughput;
+    int num_arcs;
+    struct optiq_arc *arcs;
+};
+
+struct optiq_job {
+    int id;
     int source;
-    int destination;
-    float demand;
+    int dest;
+    int demand;
+    int num_flows;
+    queue<struct optiq_flow *> flows;
 };
 
 void read_flows_from_file(char *filePath);
+
+void get_flows(int **rGraph, int num_vertices, struct optiq_job *job, int *flow_id);
+void read_flow_from_file(char *file_path, struct optiq_job *job, int num_jobs);
 
 #endif
