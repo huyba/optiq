8b63c211fce41d5cf6cc07df411cb5fa5708e6b3
Huy Bui <huybui@anl.gov>
2014-11-05 22:57:07 +0000

    update readflow

--- src/tests/readflow.c
+++ src/tests/readflow.c
@@ -1,39 +1,12 @@
-#include "iostream"
-#include <limits.h>
-#include <math.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdio.h>
 #include <queue>
+#include <limits.h>
+#include <string.h>
+
+#include <util.h>
 
 using namespace std;
-
-void rtrim(char *str)
-{
-    size_t n;
-    n = strlen(str);
-    while (n > 0 && isspace((unsigned char)str[n - 1])) {
-	n--;
-    }
-    str[n] = '\0';
-}
- 
-void ltrim(char *str)
-{
-    size_t n;
-    n = 0;
-    while (str[n] != '\0' && isspace((unsigned char)str[n])) {
-	n++;
-    }
-    memmove(str, str + n, strlen(str) - n + 1);
-}
- 
-void trim(char *str)
-{
-    rtrim(str);
-    ltrim(str);
-}
 
 struct optiq_arc {
     int ep1;
@@ -55,42 +28,6 @@ struct optiq_job {
     int num_flows;
     queue<struct optiq_flow *> flows;
 };
-
-/* Returns true if there is a path from source 's' to sink 't' in
-   residual graph. Also fills parent[] to store the path */
-bool bfs(int V, bool *visited, int **rGraph, int s, int t, int parent[])
-{
-    /* Create a visited array and mark all vertices as not visited */
-    memset(visited, 0, sizeof(bool)*V);
-
-    /* Create a queue, enqueue source vertex and mark source vertex
-       as visited*/
-    queue <int> q;
-    q.push(s);
-    visited[s] = true;
-    parent[s] = -1;
-
-    /* Standard BFS Loop */
-    while (!q.empty())
-    {
-        int u = q.front();
-        q.pop();
-
-        for (int v=0; v<V; v++)
-        {
-            if (visited[v]==false && rGraph[u][v] > 0)
-            {
-                q.push(v);
-                parent[v] = u;
-                visited[v] = true;
-            }
-        }
-    }
-
-    /* If we reached sink in BFS starting from source, then return
-       true, else false */
-    return (visited[t] == true);
-}
 
 void get_flows(int **rGraph, int num_vertices, struct optiq_job *job, int *flow_id) 
 {
