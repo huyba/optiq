2012ccefa56aca88ecc32eff53f4804adeaac0fc
Huy Bui <huybui@anl.gov>
2014-11-06 20:57:08 +0000

    readflow

--- src/tests/readflow.c
+++ src/tests/readflow.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <mpi.h>
+
 #include <flow.h>
 
 int main(int argc, char **argv)
 {
+    int world_rank, world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
     char *file_path = "flow85";
 
     int num_jobs = 85;
