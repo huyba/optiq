1d242d42bf00e6a639aab1c14512791a9936cad5
Huy Bui <huybui@anl.gov>
2014-12-01 22:01:08 +0000

    creatign more complex version

---
+++ src/tests/transport/pami_simple/pami_transport.h
@@ -0,0 +1,125 @@
+#ifndef OPTIQ_PAMI_TRANSPORT_H
+#define OPTIQ_PAMI_TRANSPORT_H
+
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+#ifdef __bgq__
+
+#include <spi/include/kernel/location.h>
+#include <spi/include/kernel/process.h>
+#include <firmware/include/personality.h>
+#include <pami.h>
+
+#endif
+
+#include "message.h"
+
+#define JOB_DONE_NOTIFICATION_DISPATCH_ID 16
+#define RECV_MESSAGE_DISPATCH_ID 17
+
+#define MAX_SHORT_MESSAGE_LENGTH 128
+
+#define NUM_SEND_COOKIES 64
+#define NUM_RECV_COOKIES 64
+
+#define RECV_MESSAGE_SIZE (8*1024*1024)
+#define NUM_RECV_MESSAGES 8
+#define NUM_SEND_MESSAGES 1024
+
+struct optiq_pami_transport;
+
+struct optiq_send_cookie {
+    struct optiq_message *message;
+    struct optiq_pami_transport *pami_transport;
+};
+
+struct optiq_recv_cookie {
+    int val;
+    void *buffer;
+};
+
+struct optiq_pami_transport {
+    struct optiq_recv_cookie recv_cookie;
+
+    vector<struct optiq_recv_cookie *> avail_recv_cookies;
+    vector<struct optiq_recv_cookie *> in_use_recv_cookies;
+
+    vector<struct optiq_send_cookie *> avail_send_cookies;
+    vector<struct optiq_send_cookie *> in_use_send_cookies;
+
+    vector<struct optiq_message *> local_messages;
+
+    vector<struct optiq_message *> in_use_recv_messages;
+    vector<struct optiq_message *> avail_recv_messages;
+    vector<struct optiq_message *> avail_send_messages;
+
+    vector<int> involved_job_ids;
+    vector<int> involved_task_ids;
+
+    //vector<struct optiq_job> *jobs;
+    int node_id;
+    int rank;
+    int size;
+    size_t num_contexts;
+
+#ifdef __bgq__
+    pami_client_t client;
+    pami_context_t context;
+    pami_endpoint_t *endpoints;
+#endif
+
+};
+
+void optiq_pami_transport_init(struct optiq_pami_transport *self);
+
+int optiq_pami_transport_send(struct optiq_pami_transport *self, struct optiq_message *message);
+
+int optiq_pami_transport_actual_send(struct optiq_pami_transport *pami_transport, void *buffer, int length, int dest, int *cookie);
+
+int optiq_pami_transport_recv(struct optiq_pami_transport *self);
+
+bool optiq_pami_transport_test(struct optiq_pami_transport *self, void *cookie);
+
+int optiq_pami_transport_process_incomming_message(struct optiq_pami_transport *self);
+
+int optiq_notify_job_done(struct optiq_pami_transport *self, vector<int> *dests); 
+
+#ifdef __bgq__
+
+void optiq_recv_done_fn(pami_context_t context, void *cookie, pami_result_t result);
+
+void optiq_send_done_fn(pami_context_t context, void *cookie, pami_result_t result);
+
+void optiq_recv_message_fn (
+        pami_context_t    context,      /**< IN: PAMI context */
+        void            *cookie,       /**< IN: dispatch cookie */
+        const void      *header,       /**< IN: header address */
+        size_t            header_size,  /**< IN: header size */
+        const void      *data,         /**< IN: address of PAMI pipe buffer */
+        size_t            data_size,    /**< IN: size of PAMI pipe buffer */
+        pami_endpoint_t   origin,
+        pami_recv_t     *recv);        /**< OUT: receive message structure */
+
+void optiq_recv_job_done_notification_fn (
+	pami_context_t    context,      /**< IN: PAMI context */
+        void            *cookie,       /**< IN: dispatch cookie */
+        const void      *header,       /**< IN: header address */
+        size_t            header_size,  /**< IN: header size */
+        const void      *data,         /**< IN: address of PAMI pipe buffer */
+        size_t            data_size,    /**< IN: size of PAMI pipe buffer */
+        pami_endpoint_t   origin,
+        pami_recv_t     *recv);        /**< OUT: receive message structure */
+
+#endif
+
+int calculate_winsize(int message_size);
+struct optiq_send_cookie* optiq_pami_transport_get_send_cookie(struct optiq_pami_transport *self);
+
+struct optiq_message* optiq_pami_transport_get_send_message(struct optiq_pami_transport *self);
+void optiq_pami_transport_return_send_message(struct optiq_pami_transport *self, struct optiq_message *message);
+bool optiq_pami_transport_forward_test(struct optiq_pami_transport *pami_transport);
+
+#endif
---
+++ src/tests/transport/pami_simple/send_recv.c
@@ -0,0 +1,125 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <string>
+
+#include <mpi.h>
+
+#include "pami_transport.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int world_rank, world_size;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    struct optiq_pami_transport pami_transport;
+    optiq_pami_transport_init(&pami_transport);
+
+    if (world_rank == 0) {
+	printf("Init transport successfully!\n");
+    }
+
+    int data_size = 4*1024*1024;
+    if (argc > 1) {
+	data_size = atoi(argv[1]) * 1024;
+    }
+
+    char *buffer = (char *)malloc(data_size);
+    for (int i = 0; i < data_size; i++) {
+	buffer[i] = i % 128;
+    }
+
+    int num_iters = 1;
+    if (argc > 2) {
+	num_iters = atoi(argv[2]);
+    }
+
+    if (world_rank == 0) {
+	printf("num_iters = %d, data_size = %d\n", num_iters, data_size);
+    }
+
+    pami_transport.recv_cookie.val = 0;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    /*Iterate the arbitration table to get the next virtual lane*/
+    uint64_t start = GetTimeBase();
+    
+    for (int i = 0; i < num_iters; i++) {
+	if (world_rank == 0) {
+	    int cookie = 1;
+	    int dest = 1;
+	    pami_transport.involved_job_ids.push_back(0);
+
+	    optiq_pami_transport_actual_send(&pami_transport, buffer, data_size, dest, &cookie);
+
+	    bool isDone = false;
+
+	    while (!isDone) {
+		isDone = optiq_pami_transport_test(&pami_transport, &cookie);
+	    }
+
+	    while(!optiq_pami_transport_forward_test(&pami_transport));
+	}
+
+	if (world_rank == 1) {
+	    pami_transport.involved_task_ids.push_back(0);
+	    pami_transport.recv_cookie.val++;
+	    pami_transport.recv_cookie.buffer = buffer;
+
+	    int isDone = 0;
+	    while (isDone == 0) {
+		isDone = optiq_pami_transport_recv(&pami_transport);
+	    }
+	}
+	//printf("Rank %d done %dth iter\n", world_rank, i);
+    }
+
+    uint64_t end = GetTimeBase();
+
+    double elapsed_time = (double)(end-start)/1.6e3;
+    double max_time;
+    MPI_Reduce(&elapsed_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    double bw = (double) data_size * 1e6 / 1024 / 1024 / elapsed_time * num_iters;
+    printf("Rank %d Elapse time = %8.0f, bw = %8.4f\n", world_rank, elapsed_time, bw);
+
+    /*if (world_rank == 0) {
+     *         double bw = (double) data_size * 1e6 / 1024 / 1024 / max_time / num_iters;
+     *                 printf("Elapse time = %8.0f, bw = %8.4f\n", max_time, bw);
+     *                     }*/
+
+    printf("Rank %d completed the test successfully\n", world_rank);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Status status;
+
+    start = GetTimeBase();
+
+    for (int i = 0; i < num_iters; i++) {
+	if (world_rank == 0) {
+	   MPI_Send(buffer, data_size, MPI_BYTE, 1, 0, MPI_COMM_WORLD);
+	}
+
+	if (world_rank == 1) {
+	    MPI_Recv(buffer, data_size, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status);
+	}
+    }
+
+    end = GetTimeBase();
+
+    elapsed_time = (double)(end-start)/1.6e3;
+
+    bw = (double) data_size * 1e6 / 1024 / 1024 / elapsed_time * num_iters;
+    printf("MPI send/recv Elapse time = %8.0f, bw = %8.4f\n", elapsed_time, bw);
+
+    MPI_Finalize();
+
+    return 0;
+}
