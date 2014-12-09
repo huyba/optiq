50316a403d6ba97adefe6e18cb4a92f817a78e4c
Huy Bui <huybui@anl.gov>
2014-12-09 20:35:05 +0000

    pattern gen for cems

---
+++ apps/cesm/alltoallv/patterngen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) 
+{
+    int world_size = 256;
+    int ratio = 64;
+
+    printf("set Nodes :=\n");
+    for (int i = 0; i < world_size; i++) {
+	printf("%d\n", i);
+    }
+
+    printf(";\n\n");
+
+    printf("set Arcs :=\n");
+
+
+    printf(";\n\n");
+
+    printf("param Capacity :=\n");
+
+
+    printf(";\n\n");
+
+    printf("param: Jobs: Source Destination Demand :=");
+
+    printf(";\n\n");
+}
