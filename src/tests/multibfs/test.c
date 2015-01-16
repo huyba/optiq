2b1532003c4032769ef6bd47954a0d81f1720d15
Huy Bui <huybui@anl.gov>
2015-01-16 17:19:36 +0000

    working on more efficency veresion of multibfs

---
+++ src/tests/multibfs/test.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include <mpi.h>
+
+#include "multibfs.h"
+
+int main(int argc, char **argv)
+{
+    struct multibfs bfs;
+
+    int size[5] = {2, 4, 4, 4, 2};
+
+    bfs.num_dims = 5;
+    for (int i = 0; i < 5; i++) {
+        bfs.size[i] = size[i];
+    }
+
+    multibfs_init(&bfs);
+
+    printf("Init done\n");
+
+    std::vector<struct path> complete_paths;
+    int num_dests = 4;
+    int dests[4] = {32, 96, 160, 224};
+
+    struct timeval t1, t2;
+
+    gettimeofday(&t1, NULL);
+
+    naive_build_paths(complete_paths, num_dests, dests, &bfs);
+
+    gettimeofday(&t2, NULL);
+
+    long int diff = (t2.tv_usec + 1000000 * t2.tv_sec) - (t1.tv_usec + 1000000 * t1.tv_sec);
+
+    printf("Build done in %ld microseconds\n", diff);
+
+    optiq_path_print_paths(complete_paths);
+
+    int num_nodes = 256;
+    optiq_path_print_stat(complete_paths, num_nodes);
+
+
+    /*2nd method*/
+    complete_paths.clear();
+
+    gettimeofday(&t1, NULL);
+    build_paths(complete_paths, num_dests, dests, &bfs);
+
+    gettimeofday(&t2, NULL);
+
+    diff = (t2.tv_usec + 1000000 * t2.tv_sec) - (t1.tv_usec + 1000000 * t1.tv_sec);
+
+    printf("Naive build done in %ld microseconds\n", diff);
+
+    optiq_path_print_paths(complete_paths);
+    optiq_path_print_stat(complete_paths, num_nodes);
+
+    return 0;
+}
+
