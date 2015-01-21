3185744b542939541d83bb34198fb71be1aaa6b0
Huy Bui <anhhuybk@gmail.com>
2015-01-21 11:22:02 -0600

    few updates

--- model/path_based/model.cmd
+++ model/path_based/model.cmd
@@ -1,8 +1,16 @@
 model model.mod;
-data model.dat;
+data 256to4_16.dat;
 option solver snopt;
 solve;
 
 display _ampl_elapsed_time;
 display _total_solve_elapsed_time;
 
+var k:=0;
+
+for {job in Jobs} {
+    for {p in Paths[job]} {
+	printf "Job_Paths_Flow %d %d %8.4f\n", job, p, Flow[job, p];
+    }
+    printf("\n");
+}
