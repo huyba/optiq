9c4d3426a05f15b60087f6712e84228e52a7e626
Huy Bui <anhhuybk@gmail.com>
2015-01-21 11:07:34 -0600

    update qsub

--- src/tests/kshortestpaths/qsub.sh
+++ src/tests/kshortestpaths/qsub.sh
@@ -1,2 +1,8 @@
 
-./a.out ./data/graph.dat 16 > model.dat
+./a.out ./data/graph.dat 64 > 256to4_64.dat
+
+cp 256to4_64.dat ../../../model/path_based/
+
+./a.out ./data/graph.dat 32 > 256to4_32.dat
+
+cp 256to4_32.dat ../../../model/path_based/
