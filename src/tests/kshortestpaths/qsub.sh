db6f079ce231850efb4b4fe5a0907dac54d5a97b
Huy Bui <anhhuybk@gmail.com>
2015-01-22 09:48:36 -0600

    update report

--- src/tests/kshortestpaths/qsub.sh
+++ src/tests/kshortestpaths/qsub.sh
@@ -1,7 +1,1 @@
-./a.out ./data/graph.dat 8 32 > model.dat
-
-#cp model.dat ../../../model/path_based/256to4_8.dat
-#git add ../../../model/path_based/256to4_8.dat
-#git commit -m "update data for model"
-#git push anl master
-#git push bb master
+./a.out ./data/graph.dat 8 3 model3.dat > stat3
