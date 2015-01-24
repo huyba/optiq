b67d232bb2e85297c5e45c62b519f699f860197a
Huy Bui <huybui@anl.gov>
2015-01-24 03:48:38 +0000

    add rputperf

--- dev/transport/pami_transport/test_rput.c
+++ dev/transport/pami_transport/test_rput.c
@@ -2,6 +2,12 @@
 #include <limits.h>
 #include <string.h>
 
+#include <mpi.h>
+
+#include <spi/include/kernel/location.h>
+#include <spi/include/kernel/process.h>
+#include <firmware/include/personality.h>
+
 #include "pami_transport.h"
 
 int main(int argc, char **argv)
@@ -9,7 +15,7 @@ int main(int argc, char **argv)
     optiq_pami_transport_init();
 
     int local_rank = 0;
-    int rput_bytes = 1024 * 1024;
+    int rput_bytes = 128 * 1024 * 1024;
     void *local_buf = malloc (rput_bytes);
 
     for (int i = 0; i < rput_bytes; i++) {
@@ -19,15 +25,19 @@ int main(int argc, char **argv)
     int remote_rank = 1;
     void *remote_buf = malloc (rput_bytes);
 
-    optiq_pami_transport_rput(local_buf, rput_bytes, local_rank, remote_buf, remote_rank);
-
-    if (pami_transport->rank == remote_rank)
+    for (int nbytes = 1024; nbytes < rput_bytes; nbytes *= 2)
     {
-	if (memcmp(local_buf, remote_buf, rput_bytes) != 0) {
-	    printf("Invalid data recv\n");
-	} else {
-	    printf("Valid data recv\n");
+	optiq_pami_transport_rput(local_buf, nbytes, local_rank, remote_buf, remote_rank);
+
+	if (pami_transport->rank == remote_rank)
+	{
+	    if (memcmp(local_buf, remote_buf, nbytes) != 0) {
+		printf("Invalid data recv\n");
+	    } else {
+		printf("Valid data recv\n");
+	    }
 	}
+
     }
 
     return 0;
