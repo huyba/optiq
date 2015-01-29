db23d11eca32389a117f7e18ed359e2a0e1ba5d4
Huy Bui <huybui@anl.gov>
2015-01-29 17:13:32 +0000

    add optiq_perf

---
+++ src/tests/multibfs/optiq_perf.h
@@ -0,0 +1,10 @@
+#ifndef OPTIQ_PERF_H
+#define OPTIQ_PERF_H
+
+struct multibfs_perf {
+    long int update_max_load_time;
+    long int add_load_time;
+    long int add_edge_path_time;
+};
+
+#endif
