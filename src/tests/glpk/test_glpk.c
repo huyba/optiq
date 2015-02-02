0ebcf748e6042b3a64b6990c2c93d5c4c270b394
Huy Bui <huybui@anl.gov>
2015-02-02 21:53:52 +0000

    edit on glpk.c

--- src/tests/glpk/test_glpk.c
+++ src/tests/glpk/test_glpk.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include <mpi.h>
 
@@ -9,35 +10,102 @@ int main(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
 
-    glp_prob *mip;
+     struct timeval t1, t2;
+    long int diff = 0L;
+
+    char *model_file = argv[1];
+    char *data_file = argv[2];
+
+    glp_prob *prob;
     glp_tran *tran;
     int ret;
-    mip = glp_create_prob();
+    prob = glp_create_prob();
     tran = glp_mpl_alloc_wksp();
-    ret = glp_mpl_read_model(tran, "model.mod", 1);
+
+    gettimeofday(&t1, NULL);
+
+    ret = glp_mpl_read_model(tran, model_file, 1);
+
+    gettimeofday(&t2, NULL);
+
+    diff = (t2.tv_usec + 1000000 * t2.tv_sec) - (t1.tv_usec + 1000000 * t1.tv_sec);
+
+    printf("Time to read model is %ld (microsecond)\n", diff);
 
     if (ret != 0)
     {
         fprintf(stderr, "Error on translating model\n");
     }
 
-    ret = glp_mpl_read_data(tran, "512to8_3.dat");
+    gettimeofday(&t1, NULL);
+
+    ret = glp_mpl_read_data(tran, data_file);
     if (ret != 0)
     {
         fprintf(stderr, "Error on translating data\n");
     }
 
+    gettimeofday(&t2, NULL);
+
+    diff = (t2.tv_usec + 1000000 * t2.tv_sec) - (t1.tv_usec + 1000000 * t1.tv_sec);
+
+    printf("Time to read data is %ld (microsecond)\n", diff);
+
+    gettimeofday(&t1, NULL);
+    
     ret = glp_mpl_generate(tran, NULL);
     if (ret != 0)
     {
         fprintf(stderr, "Error on generating model\n");
     }
 
-    glp_mpl_build_prob(tran, mip);
-    glp_simplex(mip, NULL);
-    glp_intopt(mip, NULL);
+    gettimeofday(&t2, NULL);
 
-    ret = glp_mpl_postsolve(tran, mip, GLP_MIP);
+    diff = (t2.tv_usec + 1000000 * t2.tv_sec) - (t1.tv_usec + 1000000 * t1.tv_sec);
+
+    printf("Time to generate is %ld (microsecond)\n", diff);
+
+    gettimeofday(&t1, NULL);
+
+    glp_mpl_build_prob(tran, prob);
+
+    gettimeofday(&t2, NULL);
+
+    diff = (t2.tv_usec + 1000000 * t2.tv_sec) - (t1.tv_usec + 1000000 * t1.tv_sec);
+
+    printf("Time to build is %ld (microsecond)\n", diff);
+
+    gettimeofday(&t1, NULL);
+
+    glp_simplex(prob, NULL);
+
+    gettimeofday(&t2, NULL);
+
+    diff = (t2.tv_usec + 1000000 * t2.tv_sec) - (t1.tv_usec + 1000000 * t1.tv_sec);
+
+    printf("Time to solve using simplex is %ld (microsecond)\n", diff);
+
+    gettimeofday(&t1, NULL);
+
+    glp_intopt(prob, NULL);
+
+    gettimeofday(&t2, NULL);
+
+    diff = (t2.tv_usec + 1000000 * t2.tv_sec) - (t1.tv_usec + 1000000 * t1.tv_sec);
+
+    printf("Time to solve using intopt is %ld (microsecond)\n", diff);
+
+    gettimeofday(&t1, NULL);
+
+    glp_interior(prob, NULL);
+
+    gettimeofday(&t2, NULL);
+
+    diff = (t2.tv_usec + 1000000 * t2.tv_sec) - (t1.tv_usec + 1000000 * t1.tv_sec);
+
+    printf("Time to solve using interior is %ld (microsecond)\n", diff);
+
+    ret = glp_mpl_postsolve(tran, prob, GLP_SOL);
 
     if (ret != 0)
     {
@@ -45,7 +113,7 @@ int main(int argc, char **argv)
     }
 
     glp_mpl_free_wksp(tran);
-    glp_delete_prob(mip);
+    glp_delete_prob(prob);
 
     return 0;
 
