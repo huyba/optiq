efc929f285f5cae55f58ceb808470500f955caa4
Huy Bui <anhhuybk@gmail.com>
2015-02-12 10:38:56 -0600

    add test manyto many

--- src/tests/multibfs/test_manytomany.c
+++ src/tests/multibfs/test_manytomany.c
@@ -10,32 +10,41 @@
 
 int main(int argc, char **argv)
 {
-    struct mtonbfs bfs;
+    struct multibfs bfs;
 
-    int num_dims = 5;
-    int size[5] = {2, 4, 4, 8, 2};
-    int num_nodes = 512;
+    int size[5] = {2, 4, 4, 4, 2};
+    if (argc > 5)
+    {
+        size[0] = atoi(argv[1]);
+        size[1] = atoi(argv[2]);
+        size[2] = atoi(argv[3]);
+        size[3] = atoi(argv[4]);
+        size[4] = atoi(argv[5]);
+    }
 
-    int num_dests = num_nodes/64;
+    bfs.num_dims = 5;
+    bfs.num_nodes = 1;
+    bfs.diameter = 0;
+    for (int i = 0; i < bfs.num_dims; i++) {
+        bfs.size[i] = size[i];
+        bfs.num_nodes *= size[i];
+        bfs.diameter += size[i];
+    }
+    bfs.neighbors = optiq_topology_get_all_nodes_neighbors(bfs.num_dims, size);
+
+    int num_dests = bfs.num_nodes/64;
     int *dest_ranks = (int *) malloc (sizeof(int) * num_dests);
     for (int i = 0; i < num_dests; i++) {
 	dest_ranks[i] = i * 64 + 32;
     }
 
-    int num_sources = num_nodes;
+    int num_sources = bfs.num_nodes;
     int *source_ranks = (int *) malloc (sizeof(int) * num_sources);
     for (int i = 0; i < num_sources; i++) {
 	source_ranks[i] = i;
     }
 
-    std::vector<int> *neighbors = optiq_topology_get_all_nodes_neighbors(num_dims, size);
-
-    bfs.num_dims = num_dims;
-    bfs.num_nodes = num_nodes;
-    for (int i = 0; i < 5; i++) {
-        bfs.size[i] = size[i];
-    }
-    bfs.neighbors = neighbors;
+    max_path_length = bfs.diameter/2;
 
     std::vector<struct path *> complete_paths;
     complete_paths.clear();
@@ -54,8 +63,8 @@ int main(int argc, char **argv)
 
     printf("Build done in %ld microseconds\n", diff);
 
-    optiq_path_print_paths(complete_paths);
-    optiq_path_print_stat(complete_paths, num_nodes);
+    //optiq_path_print_paths(complete_paths);
+    optiq_path_print_stat(complete_paths, bfs.num_nodes);
 
     return 0;
 }
