e71ac2fcf926e04be95a95e383be65311e8ca952
Huy Bui <huybui@anl.gov>
2015-04-26 20:59:40 +0000

    code for getting stat from paths

--- benchmarks/multipaths/yen/yen_stat.c
+++ benchmarks/multipaths/yen/yen_stat.c
@@ -12,13 +12,27 @@
 
 int main(int argc, char **argv)
 {
+    printf("hello world\n");
     char *path = argv[1];
     char *outpath = argv[2];
 
-    int start = atoi (argv[2]);
-    int end = atoi (argv[3]);
+    int start = atoi (argv[3]);
+    int end = atoi (argv[4]);
 
-    int num_nodes = atoi (argv[5]);
+    int size[5];
+    size[0] = atoi (argv[5]);
+    size[1] = atoi (argv[6]);
+    size[2] = atoi (argv[7]);
+    size[3] = atoi (argv[8]);
+    size[4] = atoi (argv[9]);
+
+    struct optiq_topology *topo = new optiq_topology();
+    optiq_topology_init_with_params(5, size, topo);
+
+    int num_nodes = topo->num_nodes;
+    int num_edges = topo->num_edges;
+
+    printf("start %d end %d numnode %d path %s outpath %s\n", start, end, num_nodes, path, outpath);
 
     char filepath[256];
     std::ofstream outfile (outpath, std::ofstream::out);
@@ -36,6 +50,7 @@ int main(int argc, char **argv)
     for (int fi = start; fi <= end; fi++)
     {
         sprintf (filepath, "%s/test%d", path, fi);
+	printf("%s", filepath);
 
         std::vector<struct job> jobs;
 	jobs.clear();
@@ -44,13 +59,19 @@ int main(int argc, char **argv)
 
 	optiq_jobs_read_from_file (jobs, paths, filepath);
 
+	printf("job size = %d, path size = %d\n", jobs.size(), paths.size());
+
 	int totalhops = 0, totalload = 0;
 
 	/* Get the jobs stat */
+	int numpaths = 0;
+
 	for (int i = 0; i < jobs.size(); i++)
 	{
 	    for (int j = 0; j < jobs[i].paths.size(); j++)
 	    {
+		numpaths += jobs[i].paths[j]->arcs.size();
+
 		if (maxhops < jobs[i].paths[j]->arcs.size())
 		{
 		    maxhops = jobs[i].paths[j]->arcs.size();
@@ -72,6 +93,7 @@ int main(int argc, char **argv)
 	    }
 	}
 
+	int numlinkused = 0;
 	for (int i = 0; i < num_nodes; i++)
 	{
 	    for (int j = 0; j < num_nodes; j++)
@@ -87,11 +109,31 @@ int main(int argc, char **argv)
 		}
 
 		totalload += load[i][j];
+
+		if (load[i][j] != 0) 
+		{
+		    numlinkused++;
+		}
 	    }
 	}
 
+	if (minhops == 0) 
+	{
+	    minhops = 1;
+	}
+
+	if (minload == 0) {
+	    minload = 1;
+	}
+
+	avghops = (double) totalhops / numpaths;
+
+	avgload = (double)totalload / num_edges;
+
+	double avgload1 = (double)totalload / numlinkused;
+
 	outfile << fi;
-	outfile << " " << maxload << " " << minload << " " << avgload << " " << medload;
+	outfile << " " << maxload << " " << minload << " " << avgload << " " << avgload1 << " " << medload;
 	outfile << " " << maxhops << " " << minhops << " " << avghops << " " << medhops;
 	outfile << std::endl;
 
@@ -104,7 +146,7 @@ int main(int argc, char **argv)
 	/* Free memory */
 	for (int i = 0; i < paths.size(); i++)
 	{
-	    free (paths[i]);
+	    delete (paths[i]);
 	}
     }
 
