ba23f8c0659410aecf0fb23bd2d89b4b7529ce8b
Huy Bui <anhhuybk@gmail.com>
2015-05-14 18:05:43 -0500

    updated results

--- docs/results/perf/single/512/stat/extract.sh
+++ docs/results/perf/single/512/stat/extract.sh
@@ -1,5 +1,13 @@
+./linkload.x opt_mp50.dat opt_hops_87_64k.dat O H 87 65536
+./linkload.x opt_mp50.dat opt_copies_87_64k.dat O C 87 65536
+./linkload.x opt_mp50.dat opt_loaddata_87_64k.dat O D 87 65536
+./linkload.x opt_mp50.dat opt_loadpath_87_64k.dat O P 87 65536
+./linkload.x opt_mp50.dat opt_hopbyte_87_64k.dat O B 87 65536
+./linkload.x opt_mp50.dat opt_hopcopy_87_64k.dat O Y 87 65536
 
 ./linkload.x heu_ml4.dat heu_hops_87_64k.dat O H 87 65536
 ./linkload.x heu_ml4.dat heu_copies_87_64k.dat O C 87 65536
 ./linkload.x heu_ml4.dat heu_loaddata_87_64k.dat O D 87 65536
 ./linkload.x heu_ml4.dat heu_loadpath_87_64k.dat O P 87 65536
+./linkload.x heu_ml4.dat heu_hopbyte_87_64k.dat O B 87 65536
+./linkload.x heu_ml4.dat heu_hopcopy_87_64k.dat O Y 87 65536
