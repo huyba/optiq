48cfe928c4c56d7d7edb21e22d547694f75b39eb
Huy Bui <huybui@anl.gov>
2015-07-29 18:07:40 +0000

    edit comments

--- dev/topology/pathreconstruct.h
+++ dev/topology/pathreconstruct.h
@@ -1,3 +1,7 @@
+/*
+ * Used to reconstruct data movement
+ * */
+
 #ifndef OPTIQ_PATH_RECONSTRUCT_H
 #define OPTIQ_PATH_RECONSTRUCT_H
 
@@ -7,8 +11,10 @@
 #include "topology.h"
 #include "path.h"
 
+/* Currently used path reconstruction method to construct path for data movement using MPI */
 void optiq_topology_path_reconstruct_new (std::vector<std::pair<int, int> > &source_dests, std::vector<struct path *> &mpi_paths);
 
+/* Obsolete method for path reconstruction */
 void optiq_topology_path_reconstruct (std::vector < std::pair <int, std::vector<int> > > &source_dests, struct optiq_topology *topo, std::vector<struct path *> &mpi_paths);
 
 #endif
--- dev/topology/topology.h
+++ dev/topology/topology.h
@@ -1,3 +1,8 @@
+/* 
+ * Contains code for topology related functions.
+ *
+ * */
+
 #ifndef OPTIQ_TOPOLOGY_BGQ_H
 #define OPTIQ_TOPOLOGY_BGQ_H
 
@@ -57,46 +62,67 @@ void optiq_topology_get_size_bgq(int *size);
 /* Get the node id*/
 int optiq_topology_get_node_id(int world_rank);
 
+/* Get a random rank to transfer as the next destination if multiple ranks are available in the next destination node - obsolete method */
 int optiq_topology_get_random_rank (int node_id);
 
+/* Calculate the number of hops between 2 nodes */
 int optiq_topology_get_hop_distance_2nodes(int node1, int node2);
 
+/* Calculate the number of hops based on ranks*/
 int optiq_topology_get_hop_distance(int rank1, int rank2);
 
+/* Compute the maximum distance between 2 sets of soures and destinations*/
 int optiq_topology_max_distance_2sets(std::vector<std::pair<int, std::vector<int> > > &source_dests);
 
+/* compute the max distance with torus included */
 int optiq_topology_max_distance_2sets_with_torus (std::vector<std::pair<int, std::vector<int> > > &source_dests);
 
+/* Get coordinates of the current node */
 int optiq_topology_get_coord(int *coord);
 
+/* Get the bridge node on bgq*/
 void optiq_topology_get_bridge_bgq(struct optiq_topology *topo);
 
+/* Compute the node id based on size and coordinate */
 int optiq_topology_compute_node_id(int num_dims, int *size, int *coord);
 
+/* Compute the neighbors of a node based on its coordinates and partition size */
 int optiq_compute_neighbors(int num_dims, int *size, int *coord, int *neighbors);
 
+/* Get all nodes neighbors */
 std::vector<int> * optiq_topology_get_all_nodes_neighbors(int num_dims, int *size);
 
+/* Get all coordinates of all nodes */
 int** optiq_topology_get_all_coords (int num_dims, int *size);
 
+/* Get torus of the given partition */
 void optiq_topology_get_torus(int *torus);
 
+/* Calculate routing order of BGQ */
 void optiq_topology_compute_routing_order_bgq(int num_dims, int *size, int *order);
 
+/* Compute how data is moved along one dimension */
 void optiq_topology_move_along_one_dimension_bgq(int num_dims, int *size, int *source, int routing_dimension, int num_hops, int direction, int **path);
 
+/* Reconstruct paths for data movement by MPI for BGQ */
 void optiq_topology_reconstruct_path_bgq(int num_dims, int *size, int *torus, int *order, int *source, int *dest, int **path);
 
+/* Remove intermediate nodes that comply with default routing algorithms */
 void optiq_topology_reduce_intermediate_nodes (std::vector<struct path *> paths, std::vector<struct path *> &reduced_paths);
 
+/* Get routing dimension order */
 void optiq_topology_route_along_dimension (int *scoord, int routing_dimension, int destcoord, struct path &p);
 
+/* Reconstruct MPI path for 1 pair of source and destination */
 void optiq_topolog_reconstruct_path (int source, int dest, struct path &p);
 
+/* Print all arcs */
 void optiq_topology_print_all_arcs(int num_dims, int *size, double cap);
 
+/* Free memory and resources */
 void optiq_topology_finalize();
 
+/* Write a graph of the given partition. Used for Yen's or paths listing algorithms */
 void optiq_topology_write_graph(struct optiq_topology *topo, int cost, char *filePath);
 
 #endif
